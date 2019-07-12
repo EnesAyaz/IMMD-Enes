46dbce8c0fc22140dc85a709f43a4118a42ed30d
Enes <enes.ayaz.1996@gmail.com>
2019-07-12 12:26:42 +0300

    Sinosoidal PWM mathemetical model

---
+++ Matlab-Simulink/PWM/SpwmTpwm.m
@@ -0,0 +1,16 @@
+%%
+clc
+clear
+%%
+f_c= 50; % carrier sinusoidal ...
+            ...frequency in Hz
+
+A_c= 1; % carrier modulation
+
+
+A_3= 1/6; % injection Third Harmonics
+
+
+f_s=10000; % switching frequency in Hz
+
+V_dc= 100; % DC link Voltage
