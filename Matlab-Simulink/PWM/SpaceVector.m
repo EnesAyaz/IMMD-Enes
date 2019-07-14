f3679b16e655ff92f713a11677a8c02e3a9a39ed
Enes <enes.ayaz.1996@gmail.com>
2019-07-14 10:28:10 +0300

    Space Vector implementatiton( not working yet)

---
+++ Matlab-Simulink/PWM/SpaceVector.m
@@ -0,0 +1,13 @@
+%%
+clc
+clear
+%%
+f_c= 50; % carrier sinusoidal ...
+            ...frequency in Hz
+
+A= 1; % reference voltage
+
+
+f_s=10000; % switching frequency in Hz
+
+V_dc= 100; % DC link Voltage
